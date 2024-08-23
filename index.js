4e5883a045591fd5d30ec8d9d572162f5186eecb
mamaagent <125088212+mamaagent@users.noreply.github.com>
2024-08-24 00:16:04 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// This is a comment - it won't be executed
+
+console.log("Hello, World!");
+
+// Define a function
+function greet(name) {
+  console.log(`Hello, ${name}!`);
+}
+
+// Call the function
+greet("Alice");
+
+
